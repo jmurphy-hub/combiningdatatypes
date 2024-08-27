87afe77c6fae8b11bf094704d6fe8d41779e4d20
John Murphy <jmurphy.org@gmail.com>
2024-08-26 21:05:24 -0400

    combining-data-types

---
+++ index.js
@@ -0,0 +1,22 @@
+console.log("cheese" + 42)
+console.log("4" + 2)
+console.log("the data type of the result when you add a number to a string in this case is 'number'")
+
+console.log("pie" + true)
+console.log(false + 8)
+console.log(true + 8)
+console.log("true is evaluated as 1 when used in a mathematical operation. false is evaluated as 0")
+console.log("when adding a string to a boolean, you receive a string back.")
+
+console.log(7 + null)
+console.log("cat" + null)
+console.log("cat" + undefined)
+
+console.log(17 - "parkbench")
+console.log(17 - true)
+console.log(17 - false)
+
+console.log("cat" / 17)
+console.log("cat" * 17)
+console.log("cat" % 17)
+console.log("the result of using strings with most arithmatic operations is not-a-number NaN")
